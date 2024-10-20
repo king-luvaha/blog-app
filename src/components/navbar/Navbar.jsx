0f44a996f462a8713eed320c3d0b426c7a23eba9
Ezra Luvaha <luvaha456@gmail.com>
2024-10-21 00:26:31 +0300

    Setup & Layout

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}> 
+      navba
+    </div>
+  )
+}
+
+export default Navbar
