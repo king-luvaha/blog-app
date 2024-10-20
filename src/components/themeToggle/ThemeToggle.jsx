34fc83b71e6d67e61f346bbbdc990e6be17c50b8
Ezra Luvaha <luvaha456@gmail.com>
2024-10-21 00:56:19 +0300

    Responsive Navbar Design

---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./themeToggle.module.css"
+
+const ThemeToggle = () => {
+  return (
+    <div className={styles.container}>
+      thim
+    </div>
+  )
+}
+
+export default ThemeToggle
