0b810385ccb5431278268df5a0555688f4b8e680
Ezra Luvaha <luvaha456@gmail.com>
2024-10-21 21:43:33 +0300

    Commit

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,22 +1,28 @@
 "use client";
-
-import React, { useState } from "react";
-import styles from "./authLinks.module.css";
 import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-  //temporary
-  const status = "authenticated";
+
+  const { status } = useSession();
 
   return (
     <>
-      {status === "notauthenticated" ? (
-        <Link href="/login" className={styles.link}>Login</Link>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write" className={styles.link}>Write</Link>
-          <span className={styles.link}>Logout</span>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
