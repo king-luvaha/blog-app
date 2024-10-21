c168a57aa70fc5cc3da920d21fe6152df1c0315e
Ezra Luvaha <luvaha456@gmail.com>
2024-10-21 13:18:16 +0300

    Blog App Featured Design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import styles from "./featured.module.css"
+import Image from 'next/image'
 
 const Featured = () => {
   return (
     <div className={styles.container}> 
-      fichad
+      <h1 className={styles.title}>
+        <b>Hey, blogger here! Discover my stories and creative ideas.</b>
+      </h1>
+
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={StyleSheet.image} />
+        </div>
+
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, reprehenderit!</h1>
+          <p className={styles.postDec}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae quae dolores earum at et quas laudantium impedit iste, consequatur itaque consequuntur libero praesentium eum nihil sapiente voluptates quasi id ipsa?
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
   )
 }
