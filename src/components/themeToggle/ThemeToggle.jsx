a93d9da984bd4a3bc035629e5cb084db34aa74b3
Ezra Luvaha <luvaha456@gmail.com>
2024-10-21 11:25:03 +0300

    Next.js Context API tutorial

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import styles from "./themeToggle.module.css"
-import Image from 'next/image'
+"use client";
+
+import React, { useContext } from "react";
+import styles from "./themeToggle.module.css";
+import Image from "next/image";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return (
-    <div className={styles.container}>
-      <Image src="/moon.png" alt="moon" width={14} height={14} />
-      <div className={styles.ball}></div>
-      <Image src="/sun.png" alt="sun" width={14} height={14}/>
-    </div>
-  )
-}
+  const { toggle, theme } = useContext(ThemeContext);
 
-export default ThemeToggle
+  console.log(theme);
+
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { left: 1, backgroundColor: "white" }
+          : { right: 1, backgroundColor: "#0F172A" }
+      }
+    >
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { left: 1, background: "#0F172A" }
+            : { right: 1, background: "white" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
+};
+
+export default ThemeToggle;
